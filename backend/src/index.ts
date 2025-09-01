bb1500be0ba31e35555a85e0f1f149cb0ac1b568
krishna3032004 <krishnagupta30mar@gmail.com>
2025-09-01 23:14:01 +0530

    add new project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,10 @@ import { authMiddleware } from './middleware/auth.js';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 
 
 // app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3000" }));
